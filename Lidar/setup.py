cdf4d788ecada66b93843eb20ad0149c8229ac8c
arnavnair <arnav.nair1209@gmail.com>
2026-09-20 17:53:30 +0530

    Create setup.py for mine_lidar_mapping package
    Initial setup script for the mine_lidar_mapping package.

---
+++ Lidar/setup.py
@@ -0,0 +1,22 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'mine_lidar_mapping'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*_launch.py')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Arnav',
+    description='LiDAR spatial mapping demo',
+    license='MIT',
+)
