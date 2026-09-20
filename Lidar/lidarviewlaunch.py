07109fa994eb79ed8af42e9e7e3d59f4048f0985
arnavnair <arnav.nair1209@gmail.com>
2026-09-20 17:51:19 +0530

    Add launch file for Lidar and RViz integration

--- Lidar/lidarviewlaunch.py
+++ Lidar/lidarviewlaunch.py
@@ -1,1 +1,31 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    rviz_config_path = os.path.join(
+        get_package_share_directory('mine_lidar_mapping'),
+        'rviz', 'lidar_view.rviz'
+    )
+
+    rplidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('rplidar_ros'),
+                'launch', 'rplidar_a1_launch.py'
+            )
+        ])
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_path],
+        output='screen'
+    )
+
+    return LaunchDescription([rplidar_launch, rviz_node])
